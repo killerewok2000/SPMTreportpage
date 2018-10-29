bd65857d46dad9efa561225ac7750853e6cdaa7c
Simon Bourdages <sibourda@microsoft.com>
2018-10-29 13:47:56 -0700

    added some test

---
+++ test_button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'test'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#test_button_container');
+ReactDOM.render(e(LikeButton), domContainer);

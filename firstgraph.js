b8aa871de7367b3f1dfe403a5a53d293d5f8210a
Simon Bourdages <sibourda@microsoft.com>
2018-10-30 14:19:48 -0700

    added colors

--- firstgraph.js
+++ firstgraph.js
@@ -90,8 +90,8 @@ function setupgraph1(data) {
             labels: ["Completed", "In progress", "Failed"],
             datasets: [{
                 label: "Status of Tasks",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: ['#107c10','#ffce56' ,'#e81123'],
+                borderColor: ['#004b1c','#ffb900' ,'#a80000'],
                 data: status,
             }]
         },

d0e93b82aeedcff777dbf556ca860fe3fedb1de4
Roshane <roshanes@users.noreply.github.com>
2018-10-30 14:48:06 -0400

    switching from array to object

--- CSVread.js
+++ CSVread.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    $.ajax({
+    /* $.ajax({
         type: "GET",
         url: "SampleCSV/SummaryReport.csv",
         dataType: "text",
         success: function(data) {processData(data);}
      });
+     */
 });
 
 function processData(allText) {
@@ -16,12 +17,38 @@ function processData(allText) {
         var data = allTextLines[i].split(',');
         if (data.length == headers.length) {
 
-            var tarr = [];
+            /*var tarr;
             for (var j=0; j<headers.length; j++) {
                 tarr.push(headers[j]+":"+data[j]);
             }
             lines.push(tarr);
+            */
+
+            var tarr = {
+                source: data[0],
+                dest: data[1],
+                status: data[2],
+                totalBytes: data[3],
+                totalGB: data[4],
+                migratedBytes: data[5],
+                migratedGB: data[6],
+                notMigratedGB: data[7],
+                itemsScanned: data[8],
+                itemsReady: data[9],
+                itemsMigrated: data[10],
+                itemsNotMigrated: data[11],
+                warningCount: data[12],
+                startTime: data[13],
+                endTime: data[14],
+                duration: data[15],
+                transferRate: data[16],
+                roundNumber: data[17],
+                workflowId: data[18],
+                taskId: data[19],
+                logPath: data[20]
+            };
+            
         }
     }
-    // alert(lines);
+    //alert(lines);
 }
